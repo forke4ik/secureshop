014d210d382a0e59fd78be1b22820e8cdaee8663
forke4ik <01forkee09@gmail.com>
2025-08-22 18:49:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,9 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Owner IDs
-OWNER_ID_1 = int(os.getenv('OWNER_ID_1')) if os.getenv('OWNER_ID_1') else None
-OWNER_ID_2 = int(os.getenv('OWNER_ID_2')) if os.getenv('OWNER_ID_2') else None
+OWNER_ID_1 = int(os.environ.get('OWNER_ID_1', 0)) # Замініть 0 на реальний ID, якщо потрібно за замовчуванням
+OWNER_ID_2 = int(os.environ.get('OWNER_ID_2', 0)) # Замініть 0 на реальний ID, якщо потрібно за замовчуванням
+SECURE_SUPPORT_ID = int(os.environ.get('SECURE_SUPPORT_ID', 0)) # Новий ID менеджера
 OWNER_IDS = [id for id in [OWNER_ID_1, OWNER_ID_2] if id is not None]
 
 # NOWPayments API
@@ -24,4 +25,5 @@ NOWPAYMENTS_IPN_SECRET = os.getenv('NOWPAYMENTS_IPN_SECRET')
 PAYMENT_CURRENCY = "UAH"  # Изменено с USD на UAH
 
 # Card number for manual payment simulation
+
 CARD_NUMBER = "5355 2800 4715 6045"
